e380ccd417fe2a42d1c1049d21159a6d1b0a4632
Josakko <112590914+Josakko@users.noreply.github.com>
2023-09-20 21:24:22 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,8 +6,6 @@
 #  exit 1
 #fi
 
-path=exec pwd
-
 #repos
 #sudo cp files/sources.list /etc/apt/sources.list
 echo "deb-src http://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware" | sudo tee -a /etc/modprobe.d/blacklist.conf
